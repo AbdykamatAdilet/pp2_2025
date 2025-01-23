399fc6091e97c9c9be20fbd4c0f62fc189ee3ba7
AbdykamatAdilet <mcpeadilet@gmail.com>
2025-01-23 22:26:23 +0300

    Variables

---
+++ lab1/4.py
@@ -0,0 +1,16 @@
+#Python variables: In this file i will practise all type of variables!
+
+x, y, z = "Aktau", "Almaty", "Astana"
+print(x) #Result is: Aktau
+print(y) #Result is: Almaty
+print(z) #Result is: Astana
+
+q = "I"
+w = "am"
+e = "the"
+r = "best"
+t = "in"
+u = "the"
+p = "world"
+print(q,w,e,r,t,u,p) #Result is: I am the best in the world
+print(q+w+e+r+t+u+p) #Result is: Iamthebestintheworld
